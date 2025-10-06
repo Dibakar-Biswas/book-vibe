383190dbe3b4ea852b2dfed8ac975e029d7ba992
Dibakar-Biswas <dibakarbiswas742@gmail.com>
2025-10-06 18:29:01 +0600

    added sorting

--- src/pages/ReadList/ReadList.jsx
+++ src/pages/ReadList/ReadList.jsx
@@ -6,18 +6,51 @@ import { getStoredBook } from "../../Utility/addToDB";
 import Book from "../Book/Book";
 
 const ReadList = () => {
-    const [readList, setReadList] = useState([])
-    const data = useLoaderData();
-    console.log(data)
+  const [readList, setReadList] = useState([]);
+  const [sort, setSort] = useState("")
+  const data = useLoaderData();
 
-    useEffect(() => {
-        const storedBookData = getStoredBook()
-        const ConvertedStoredBooks = storedBookData.map(id => parseInt(id));
-        const myReadList = data.filter(book => ConvertedStoredBooks.includes(book.bookId));
-        setReadList(myReadList)
-    },[])
-    return (
+  useEffect(() => {
+    const storedBookData = getStoredBook();
+    const ConvertedStoredBooks = storedBookData.map((id) => parseInt(id));
+    const myReadList = data.filter((book) =>
+      ConvertedStoredBooks.includes(book.bookId)
+    );
+    setReadList(myReadList);
+  }, []);
+
+
+
+
+  const handleSort = (type) => {
+    setSort(type)
+
+    if(type === "pages"){
+        const sortedByPage = [...readList].sort((a,b) => a.totalPages - b.totalPages);
+        setReadList(sortedByPage)
+        console.log(sortedByPage)
+    }
+    if(type === "ratings"){
+        const sortedByRatings = [...readList].sort((a,b) => a.rating - b.rating);
+        setReadList(sortedByRatings)
+    }
+  }
+
+
+
+  return (
     <div>
+      <details className="dropdown">
+        <summary className="btn m-1">Sort By : {sort? sort:""}</summary>
+        <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+          <li>
+            <a onClick={() => handleSort("pages")}>pages</a>
+          </li>
+          <li>
+            <a onClick={() => handleSort("ratings")}>ratings</a>
+          </li>
+        </ul>
+      </details>
       <Tabs>
         <TabList>
           <Tab>Read Book List</Tab>
@@ -27,9 +60,9 @@ const ReadList = () => {
         <TabPanel>
           <h2>Book I Read {readList.length}</h2>
 
-          {
-            readList.map(b => <Book key={b.bookId} singleBook={b}></Book>)
-          }
+          {readList.map((b) => (
+            <Book key={b.bookId} singleBook={b}></Book>
+          ))}
         </TabPanel>
         <TabPanel>
           <h2>My Wish List</h2>
