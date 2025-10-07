f92a14a0d9ff24849f886760226f3506869927c3
Dibakar-Biswas <dibakarbiswas742@gmail.com>
2025-10-08 00:04:40 +0600

    update an error

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -16,7 +16,7 @@ export const router = createBrowserRouter([
     children: [
         {
             index: true,
-            loader: () => fetch('booksData.json'),
+            loader: () => fetch('/booksData.json'),
             path: "/",
             Component: Home
         },
@@ -26,12 +26,12 @@ export const router = createBrowserRouter([
         },
         {
             path: 'readList',
-            loader: () => fetch('booksData.json'),
+            loader: () => fetch('/booksData.json'),
             Component: ReadList
         },
         {
             path: '/bookDetails/:id',
-            loader: () => fetch('./booksData.json'),
+            loader: () => fetch('/booksData.json'),
             Component: BookDetails
         }
     ]
